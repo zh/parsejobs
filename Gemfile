72b5b39aecf800ff566a2f2acb3fc209317742dd
stoyan <stoyan@kndb.jp>
2012-02-20 18:26:42 +0900

    Rack::Flash for messages; specs stopped working

--- Gemfile
+++ Gemfile
@@ -2,10 +2,14 @@ source :rubygems
 
 gem 'rake'
 gem 'json'
+gem 'rack', '~> 1.3.6'
+gem 'async-rack'
 gem 'sinatra'
 gem 'sinatra-synchrony'
+gem 'rack-flash'
 gem 'faraday'
 gem 'thin'
+gem 'erubis'
 
 group :test do
   gem 'rack-test'
--- app.rb
+++ app.rb
@@ -1,17 +1,25 @@
 # encoding: utf-8
 
-%w[json sinatra em-synchrony faraday].map {|lib| require lib}
+%w[json sinatra sinatra/synchrony faraday rack-flash].map {|lib| require lib}
 Faraday.default_adapter = :em_synchrony
 
 ENV['RACK_ENV'] ||= "production"
 APP_ROOT = File.expand_path(File.dirname(__FILE__))
 
 class ParseJobs < Sinatra::Base
+
+  register Sinatra::Synchrony
+
+  use Rack::Session::Cookie, :secret => 'please_change_this'
+  use Rack::Flash
+  use Rack::CommonLogger
+
   configure do
-    disable :sessions
+    enable :sessions
     enable :logging
     set :environment, ENV['RACK_ENV']
     set :public_folder, File.join(APP_ROOT, "public")
+    set :server, %w[thin webrick]
   end
 
   get '/' do
@@ -23,20 +31,28 @@ class ParseJobs < Sinatra::Base
   end
 
   post '/apply' do
-    halt 404, "Invalid data" unless params[:name] and params[:email]
-    halt 404, "Why Parse?" unless params[:about] and not params[:about].empty?
-    halt 404, "Provide some links" unless params[:links] and not params[:links].empty?
-    payload = {:name=>params[:name], :email=>params[:email], :about=>params[:about]}
-    links = []
-    params[:links].each_line { |line| links << line.chomp }
-    payload[:urls] = links.compact.delete_if {|x| x.empty? }
-    api = Faraday.new(:url => 'https://www.parse.com')
-    response = api.post do |req|
-      req.url '/jobs/apply'
-      req.headers['Content-Type'] = 'application/json'
-      req.body = payload.to_json
+    begin
+      raise "Invalid data" unless not params[:name].empty? and not params[:email].empty?
+      raise "Tell us why Parse?" unless params[:about] and not params[:about].empty?
+      raise "Provide us with some links" unless params[:links] and not params[:links].empty?
+      payload = {:name=>params[:name], :email=>params[:email], :about=>params[:about]}
+      links = []
+      params[:links].each_line { |line| links << line.chomp }
+      payload[:urls] = links.compact.delete_if {|x| x.empty? }
+      api = Faraday.new(:url => 'https://www.parse.com')
+      response = api.post do |req|
+        req.url '/jobs/apply'
+        req.headers['Content-Type'] = 'application/json'
+        req.body = payload.to_json
+      end
+      if response.status.to_i == 200
+        flash[:notice] = "Request sent. Relax and wait for answer."
+      else
+        flash[:error] = response['Status']
+      end
+    rescue Exception => ex
+      flash[:error] = ex.to_s
     end
-    puts response.inspect
     redirect '/'
   end
 
